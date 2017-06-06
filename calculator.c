5e0cbfefb3191788b27ad4953e328abbe0b09c43
Mashrafi Mohammad <whitehathacker2005@gmail.com>
2017-06-06 14:50:18 +0600

    Update calculator.c
    This is an extended version.
    In the previous code, I used the integer variable. Now I've used the double variable. So, the application will provide more 
    accurate results

--- calculator.c
+++ calculator.c
@@ -3,21 +3,30 @@
   Author:Sheikh Mashrafi Mohammad
   Language Used:"C"
 */
+
 #include <stdio.h>
 #include <conio.h>
 
-int main()
+int main()  // Main function
 {
-   int num1, num2;
-   printf("Please enter a number: ");
-   scanf("%d", &num1);
-   printf("Please enter another number: ");
-   scanf("%d", &num2);
+    double num1, num2;  //  Variable declared
 
-   printf("%d+%d=%d\n", num1, num2, num1+num2);
-   printf("%d-%d=%d\n", num1, num2, num1-num2);
-   printf("%d*%d=%d\n", num1, num2, num1*num2);
-   printf("%d/%d=%d\n", num1, num2, num1/num2);
+    printf("Please enter a number: ");  // Print function
+    scanf("%lf", &num1); // Scan function
 
-   return 0;
+    printf("Please enter another number: "); // Print function
+    scanf("%lf", &num2); // Scan function
+
+    double sum, minusresult, product, dividebyresult;  //  Variable declared
+    sum=num1+num2;
+    minusresult=num1-num2;
+    product=num1*num2;
+    dividebyresult=num1/num2;
+
+    printf("The sum of your entered numbers is: %lf\n", sum);                           // Print function
+    printf("The difference of your entered numbers is: %lf\n", minusresult);           // Print function
+    printf("The product of your entered numbers is: %lf\n", product);                 // Print function
+    printf("The division result of your entered numbers is: %lf\n", dividebyresult); // Print function
+
+    return 0;
 }
